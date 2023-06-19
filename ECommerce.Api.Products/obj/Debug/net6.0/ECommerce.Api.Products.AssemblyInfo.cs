9c94214d80f9d116a180ba8a2a892a177fa1a216
MRElshafei <67318741+MRElshafei@users.noreply.github.com>
2023-06-20 00:40:00 +0300

    add docker to my micro services and run it using docker

--- ECommerce.Api.Products/obj/Debug/net6.0/ECommerce.Api.Products.AssemblyInfo.cs
+++ ECommerce.Api.Products/obj/Debug/net6.0/ECommerce.Api.Products.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9bccb800-43ee-4eaf-921d-3fd07bf71632")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Api.Products")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
